848cf72d698cdbf8f5b6c9834bed6566ee59f88e
Rashedul Hasan Rijul <rashedulhasanrijul@gmail.com>
2018-03-03 05:31:52 +0800

    refs #5: blank implementation of interfaces

--- src/main/java/com/rijul/pacman/service/UserInputService.java
+++ src/main/java/com/rijul/pacman/service/UserInputService.java
@@ -1,7 +1,7 @@
 package com.rijul.pacman.service;
 
 public interface UserInputService {
-	
+
 	public char getKey();
 
 }
---
+++ src/main/java/com/rijul/pacman/service/impl/ScorerServiceImpl.java
@@ -0,0 +1,15 @@
+package com.rijul.pacman.service.impl;
+
+import com.rijul.pacman.model.Board;
+import com.rijul.pacman.model.Player;
+import com.rijul.pacman.service.ScorerService;
+
+public class ScorerServiceImpl implements ScorerService {
+
+	@Override
+	public void updatePlayerScore(Player p, Board b) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
